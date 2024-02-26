443867454ff42a887db63316744e3f32261aa832
trunglk-ibl <trunglk@blockchainlabs.asia>
2024-02-26 10:46:47 +0700

    update source

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'signDot.js',
+    filename: 'dotLib.js',
     path: path.resolve(__dirname, 'dist'),
+    library: 'dotLib',
+    libraryTarget: 'umd',
+    globalObject: 'this',
   },
 };
